67eb2c93051e3556ba7cfc34b77de799ec013a32
intodust <deepakraja.nia@gmail.com>
2015-08-20 19:52:06 +0530

    Signed-off-by: intodust <deepakraja.nia@gmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code has two functions sources, first one creates the matrix and returns
+## list of functions,  
+## second function takes list of functions as argument and check if inverse is 
+##cached
+## if yes then it uses the cache and returns the value, if not then it calculates
+## the inverse and sets to cache and returns the value.
 
-## Write a short comment describing this function
-#changes more
+
+## makcecacheMatrix takes matrix as argument, and returns list of functions
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
+      orginalmatrix<- x
+      inversematrix<- NULL
+      
+      set <- function(y) {
+            x <<- y
+            inversematrix <<- NULL
+      }
+      
+      get <- function() orginalmatrix
+
+      
+      setcache<- function(inverse) inversematrix<<- inverse
+      
+      getcache<- function() inversematrix
+      
+      list(set=set, get= get, setcache= setcache, getcache= getcache)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve take list of function as argument and
+##returns the inverse of matrix x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
       
+      inverse<- x$getcache()
+      if(!is.null(inverse)){
+            message("getting cached inverse matrix")
+            return(inverse)
+      }
+      
+      original<- x$get()
+      inverse<- solve(original)
+      x$setcache(inverse)
+      inverse
 }
---
+++ calminus.R
@@ -0,0 +1,11 @@
+calminus <- function(x, t){
+      u<- x$getx()
+      if(t==1){
+            x$minusone(u)
+      }
+      if(t==2){
+           x$minustwo(u)
+      
+      
+}
+}
